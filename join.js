bf0e3172c235bbe6c2b408fc303b33c8d765fb08
anamika3094 <146820914+anamika3094@users.noreply.github.com>
2024-04-09 22:38:12 +0530

    Add files via upload

---
+++ join.js
@@ -0,0 +1,14 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+  function openSignUpForm() {
+      document.getElementById("signUpForm").style.display = "block";
+  }
+  
+  function closeSignUpForm() {
+      document.getElementById("signUpForm").style.display = "none";
+  }
